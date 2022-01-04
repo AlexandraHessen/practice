cf1d9f46f722120a3953ac3e8d83e72326e21515
AlexandraHessen <xl_energy@mail.ru>
2022-01-04 20:30:45 +0300

    new Function

---
+++ newFunc.js
@@ -0,0 +1,3 @@
+new Function('a', 'b', 'return a + b'); // стандартный синтаксис
+new Function('a,b', 'return a + b'); // через запятую в одной строке
+new Function('a , b', 'return a + b'); // через запятую с пробелами в одной строке
