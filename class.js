68c0732f340e5b8ac06b4405c5c51ad258c36726
AlexandraHessen <xl_energy@mail.ru>
2022-01-09 22:34:04 +0300

    class

---
+++ class.js
@@ -0,0 +1,16 @@
+class Animal {
+    constructor(name) {
+      this.speed = 0;
+      this.name = name;
+    }
+    run(speed) {
+      this.speed = speed;
+      alert(`${this.name} бежит со скоростью ${this.speed}.`);
+    }
+    stop() {
+      this.speed = 0;
+      alert(`${this.name} стоит.`);
+    }
+  }
+  
+  let animal = new Animal("Мой питомец");
