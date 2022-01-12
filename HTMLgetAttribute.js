128486f778e5f9b24128470729e6a9408255a191
AlexandraHessen <xl_energy@mail.ru>
2022-01-12 22:23:23 +0300

    HTMLgetAttribute

---
+++ HTMLgetAttribute.js
@@ -0,0 +1,11 @@
+<img src='Examples/fountain.jpg' id=AA>
+
+<script>
+
+    "use strict";
+
+    var elem=document.getElementById('AA');
+    console.log( 'DOM-атрибут src = ' + elem.src );
+    console.log( 'HTML-атрибут src = ' + elem.getAttribute('src') );
+  
+</script>
