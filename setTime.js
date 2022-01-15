f5b6354bdbad051d35e0086444ffd9158590f6db
AlexandraHessen <xl_energy@mail.ru>
2022-01-15 22:41:51 +0300

    setTimeout

--- setTime.js
+++ setTime.js
@@ -3,3 +3,22 @@ alert(timerId); // идентификатор таймера
 
 clearTimeout(timerId);
 alert(timerId); // тот же идентификатор (не принимает значение null после отмены)
+
+var counter;
+var tickTimer;
+
+function start() {
+    setTimeout(stop,1000);
+    counter=0;
+    tickTimer=setTimeout(tick,0)
+}
+
+function tick() {
+    counter++;
+    tickTimer=setTimeout(tick,0)
+}
+
+function stop() {
+    clearTimeout(tickTimer);
+    alert('срабатываний в секунду: '+counter);
+}
