94ce0d9ae03c8e93ebfb1c6ef61ecdb583a5249d
AlexandraHessen <xl_energy@mail.ru>
2022-01-15 22:41:17 +0300

    setInterval

--- setInterval.js
+++ setInterval.js
@@ -3,3 +3,23 @@ let timerId = setInterval(() => alert('tick'), 2000);
 
 // остановить вывод через 5 секунд
 setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);
+
+
+
+var counter;
+var tickTimer;
+
+function start() {
+    setTimeout(stop,1000);
+    counter=0;
+    tickTimer=setInterval(tick,0)
+}
+
+function tick() {
+    counter++;
+}
+
+function stop() {
+    clearInterval(tickTimer);
+    alert('срабатываний в секунду: '+counter);
+}
