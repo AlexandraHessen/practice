d933467d21c15d8ecd39e1b1a0a1df9716b7e589
AlexandraHessen <xl_energy@mail.ru>
2022-01-20 22:23:25 +0300

    createTimerPromise

---
+++ createTimerPromise.js
@@ -0,0 +1,22 @@
+<script>
+
+    "use strict";
+
+    function createTimerPromise(result) {
+
+        return new Promise( (resolve,reject) => {
+            console.log("я создан, запущен...");
+            setTimeout( () => {
+                console.log("я решён!");
+                resolve(result);
+                }, 1000);
+        });
+
+    }
+
+    createTimerPromise(5).then( result => {
+            console.log("промис решён с результатом "+result);
+        }
+    );
+
+</script>
