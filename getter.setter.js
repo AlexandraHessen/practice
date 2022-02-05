818b23827265390eb5af6bb4dc5e1c6e91132681
AlexandraHessen <xl_energy@mail.ru>
2022-02-05 10:11:15 +0300

    getter/setter

--- getter.setter.js
+++ getter.setter.js
@@ -16,3 +16,47 @@ let user = {
   
   alert(user.name); // Alice
   alert(user.surname); // Cooper
+
+    let obj = {
+        get name() {
+            return `...`;
+        // геттер, срабатывает при чтении obj.name
+        },
+    };
+
+    let obj = {
+        set name(value) {
+            // сеттер, срабатывает при записи obj.name = value
+          }
+    };
+
+let user = {
+    name: "John",
+    surname: "Smith",
+  
+    get fullName() {
+      return this.firstName + " " + this.lastName;
+      // return `${this.name} ${this.surname}`;
+    },
+  
+    set fullName(value) {
+      [this.name, this.surname] = value.split(" ");
+    }
+}
+
+let obj = {
+    set name(value) {
+        this.name=value;
+    // сеттер, срабатывает при записи obj.name = value
+    },
+};
+
+function ClassName() {
+    let self=this
+    this.svoistvo=value; 
+  
+    this.metod=function() { 
+    }
+  }
+
+let objClass = new ClassName
