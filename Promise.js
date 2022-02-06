985e60cec54a0706d308489636c3b2323b4eaa90
AlexandraHessen <xl_energy@mail.ru>
2022-02-06 19:47:59 +0300

    promise

--- Promise.js
+++ Promise.js
@@ -1,24 +1,63 @@
-<script>
+	
+"use strict";
 
-    "use strict";
+console.log('точка 1');
 
-    console.log('точка 1');
+let promise = new Promise( (resolve,reject) => {
 
-    var pr=new Promise( (resolve,reject) => {
-        console.log("я создан, запущен...");
+});
+
+
+promise.then( result => {
+    
+    }
+);
+
+
+console.log('точка 1'); //1
+
+var pr=new Promise( (resolve,reject) => {
+    console.log("я создан, запущен..."); //2 !! эта функция будет вызвана сразу!
+    setTimeout( () => {                  //  а вот когда уже придет результат будут вызваны функции результата
+        console.log("я решён!"); //5
+        resolve(5);
+        }, 1000);
+});
+
+console.log('точка 2'); //3
+
+pr.then( result => {
+        console.log("промис решён с результатом "+result); //6
+    }
+);
+
+console.log('точка 3'); //4
+
+
+function createTimerPromise(result) {
+
+    return new Promise( (resolve,reject) => {
+    });
+
+}
+
+createTimerPromise().then( result => {
+    }
+);
+
+function createTimerPromise(result) {
+
+    return new Promise( (resolve,reject) => {
+        console.log("я создан, запущен..."); //1
         setTimeout( () => {
-            console.log("я решён!");
-            resolve(5);
+            console.log("я решён!"); //2
+            resolve(result);
             }, 1000);
     });
 
-    console.log('точка 2');
+}
 
-    pr.then( result => {
-            console.log("промис решён с результатом "+result);
-        }
-    );
-
-    console.log('точка 3');
-
-</script>
+createTimerPromise(5).then( result => {
+        console.log("промис решён с результатом "+result); //3
+    } 
+);
