68698d099bb81b20f3c0174ec04f899864dfcb1f
AlexandraHessen <xl_energy@mail.ru>
2022-02-06 19:46:59 +0300

    arguments

--- arguments.js
+++ arguments.js
@@ -12,3 +12,19 @@ function showName() {
   
   // Вывод: 1, Илья, undefined (второго аргумента нет)
   showName("Илья");
+
+  function func(a) {
+    console.log(arguments.length); //2
+    for ( var i=0; i<arguments.length; i++ )
+      console.log(arguments[i]);
+  }
+  
+  func("aaa","bbb");
+
+  function func(a,b,...rest) {
+    console.log(rest.length); // 2   //  rest - массив всех аргументов ПОСЛЕ a и b
+    for ( var i=0; i<rest.length; i++ )
+      console.log(rest[i]); // "e","f"
+  }
+  
+  func("с","d","e","f");
