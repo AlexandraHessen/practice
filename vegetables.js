ccef9a0cf1563cdb226f99d39c857546fd35b924
AlexandraHessen <xl_energy@mail.ru>
2022-03-24 22:39:06 +0300

    vegetables

--- vegetables.js
+++ vegetables.js
@@ -13,3 +13,13 @@ console.log(vegetables);
 
 console.log(removedItems);
 // ["Репа", "Редиска"]
+
+let fruits = ["Банан"]
+
+let arr = fruits; // копируется по ссылке (две переменные ссылаются на один и тот же массив)
+
+alert( arr === fruits ); // true
+
+arr.push("Груша"); // массив меняется по ссылке
+
+alert( fruits ); // Банан, Груша - теперь два элемента
