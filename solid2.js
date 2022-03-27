165e1071fd8ea6b776f332cd107051cd3fdb0efd
AlexandraHessen <xl_energy@mail.ru>
2022-03-27 23:39:25 +0300

    solid2

--- solid2.js
+++ solid2.js
@@ -1,24 +1,24 @@
 //Open for Extension, Closed for Modification
 class Guitar {
-    constructor(options) {
-      this.type = options.type
-      this.model = options.model
-      this.strings = options.strings
-    }
+  constructor(options) {
+    this.type = options.type
+    this.model = options.model
+    this.strings = options.strings
   }
-  
-  class RockAndRollGuitar extends Guitar {
-    constructor(options) {
-      super(options)
-      this.color = options.color
-    }
+}
+
+class RockAndRollGuitar extends Guitar {
+  constructor(options) {
+    super(options)
+    this.color = options.color
   }
-  
-  const guitar = new RockAndRollGuitar({
-    type: 'Electric',
-    model: 'Electric Guitar #8',
-    strings: 8,
-    color: 'Cool color'
-  })
-  
-  console.log(guitar)
+}
+
+const guitar = new RockAndRollGuitar({
+  type: 'Electric',
+  model: 'Electric Guitar #8',
+  strings: 8,
+  color: 'Cool color'
+})
+
+console.log(guitar)
