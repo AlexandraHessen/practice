5c6e95fc11427b97d2b87b32e1101987fa88dee5
AlexandraHessen <xl_energy@mail.ru>
2022-04-10 23:12:48 +0300

    clear arr

---
+++ clearArr.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4, 5];
+
+arr.length = 2; // укорачиваем до двух элементов
+alert( arr ); // [1, 2]
+
+arr.length = 5; // возвращаем length как было
+alert( arr[3] ); // undefined: значения не восстановились
+arr.length = 0;
