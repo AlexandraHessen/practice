75fa1d5c1f024a0067d15ee34d24dbfabb52a634
AlexandraHessen <xl_energy@mail.ru>
2022-04-20 23:20:30 +0300

    user

---
+++ user.js
@@ -0,0 +1,18 @@
+function User(name) {
+    this.name = name;
+  
+    this.sayHi = function() {
+      alert( "Меня зовут: " + this.name );
+    };
+  }
+  
+  let vasya = new User("Вася");
+  
+  vasya.sayHi(); // Меня зовут: Вася
+  
+  /*
+  vasya = {
+     name: "Вася",
+     sayHi: function() { ... }
+  }
+  */
