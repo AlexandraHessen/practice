f3b2f4b161d653497f007b3355f591803b148192
AlexandraHessen <xl_energy@mail.ru>
2022-05-14 23:48:06 +0300

    onVisibilityChange

---
+++ onVisibilityChange.js
@@ -0,0 +1,10 @@
+function onVisibilityChange() {
+    if (document.visibilityState === 'hidden') {
+      this._autoplay('stop');
+    } else if (document.visibilityState === 'visible') {
+      if (this._config.loop) {
+        this._autoplay();
+      }
+    }
+  }
+  document.addEventListener('visibilitychange', onVisibilityChange.bind(this));
