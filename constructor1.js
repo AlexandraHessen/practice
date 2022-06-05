05a80edc1be9d5c1e2083643ef38776eb7f39ca8
AlexandraHessen <xl_energy@mail.ru>
2022-06-05 23:52:34 +0300

    constructor1

---
+++ constructor1.js
@@ -0,0 +1,15 @@
+class User {
+
+    constructor(name) {
+      this.name = name;
+    }
+  
+    sayHi() {
+      alert(this.name);
+    }
+  
+  }
+  
+  // Использование:
+  let user = new User("Иван");
+  user.sayHi();
