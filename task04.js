2f7a7dde0a06a0cd94d874111d541c4b2eb182a8
AlexandraHessen <xl_energy@mail.ru>
2022-06-12 22:51:30 +0300

    task

---
+++ task04.js
@@ -0,0 +1,11 @@
+function sayHiBye(firstName, lastName) {
+
+    // функция-помощник, которую мы используем ниже
+    function getFullName() {
+      return firstName + " " + lastName;
+    }
+  
+    alert( "Hello, " + getFullName() );
+    alert( "Bye, " + getFullName() );
+  
+  }
