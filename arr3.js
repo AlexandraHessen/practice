49e24eb6317236e6438f9a48100b8eb336adac2c
AlexandraHessen <xl_energy@mail.ru>
2022-07-06 23:53:54 +0300

    arr

---
+++ arr3.js
@@ -0,0 +1,6 @@
+var a1 = [1, 1, 2]; // true
+var a2 = [3, 2, 0]; // true
+var a3 = [1, 1, 1]; // true
+var a4 = [1, 2, 0]; // false
+var a5 = [1, 2, 3, 1, 2, 3]; // false
+var a6 = [4, 1, 1, 4]; // false
