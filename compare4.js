2bac17547372bba664b6f308935b8e6781a5995f
AlexandraHessen <xl_energy@mail.ru>
2022-07-15 23:18:42 +0300

    compare4

---
+++ compare4.js
@@ -0,0 +1,18 @@
+var a = {
+    key: 'foo'
+};
+var b = {
+    key: 'bar'
+};
+var c = {};​
+c[a] = 123;
+c[b] = 345;
+console.log(c[a], c[b]);
+
+
+
+var a = 42;
+var b = "foo";​
+a < b;
+a > b;
+a == b;
