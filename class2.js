c67d3cde97f379555df62a502f57936aab383fc3
AlexandraHessen <xl_energy@mail.ru>
2022-07-15 23:16:43 +0300

    class2

---
+++ class2.js
@@ -0,0 +1,13 @@
+function SomeClassOne() {
+    this.val = 'One';
+}​
+var o = new SomeClassOne();
+console.log(o.val);
+
+​
+function SomeClassTwo() {
+    this.val = 'Two';
+    return this.val;
+}​
+o = new SomeClassTwo();
+console.log(o.val);
