b537cb0503435d4be9798eb1df984db7cd54af2f
AlexandraHessen <xl_energy@mail.ru>
2022-07-28 22:35:39 +0300

    arr_isArray

---
+++ arr_isArray.js
@@ -0,0 +1,16 @@
+Array.isArray([]);
+Array.isArray([1]);
+Array.isArray(new Array());
+// Малоизвестный факт: Array.prototype сам является массивом:
+Array.isArray(Array.prototype);
+
+// Все следующие вызовы вернут false
+Array.isArray();
+Array.isArray({});
+Array.isArray(null);
+Array.isArray(undefined);
+Array.isArray(17);
+Array.isArray("Array");
+Array.isArray(true);
+Array.isArray(false);
+Array.isArray({ __proto__: Array.prototype });
