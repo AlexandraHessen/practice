a3ff765cd47c4dc9e7eaad1066000351a202181c
AlexandraHessen <xl_energy@mail.ru>
2022-07-29 09:57:29 +0300

    arr_keys

--- arr_keys.js
+++ arr_keys.js
@@ -11,7 +11,7 @@ for (const key of iterator) {
 
 
 var arr = ['a', 'b', 'c'];
-var iterator = arr.keys();
+var iterator2 = arr.keys();
 
 console.log(iterator.next()); // { value: 0, done: false }
 console.log(iterator.next()); // { value: 1, done: false }
