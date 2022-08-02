a322c2ba317cc544cf01c1fd507671760c1c703d
AlexandraHessen <xl_energy@mail.ru>
2022-08-02 23:27:41 +0300

    arr_pop

---
+++ arr_pop.js
@@ -0,0 +1,9 @@
+var myFish = ['ангел', 'клоун', 'мандарин', 'хирург'];
+
+console.log(myFish); // ['ангел', 'клоун', 'мандарин', 'хирург']
+
+var popped = myFish.pop();
+
+console.log(myFish); // ['ангел', 'клоун', 'мандарин']
+
+console.log(popped); // 'хирург'
