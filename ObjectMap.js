3d7284c7c6452c7b78ee4d1f9b3fc81655d2cf4d
AlexandraHessen <xl_energy@mail.ru>
2022-08-04 17:36:49 +0300

    ObjectMap

---
+++ ObjectMap.js
@@ -0,0 +1,6 @@
+var obj = { foo: "bar", baz: 42 };
+var map = new Map(Object.entries(obj));
+console.log(map); // Map { foo: "bar", baz: 42 }
+
+const obj = { foo: 'bar', baz: 42 };
+Object.entries(obj).forEach(([key, value]) => console.log(`${key}: ${value}`)); // "foo: bar", "baz: 42"
