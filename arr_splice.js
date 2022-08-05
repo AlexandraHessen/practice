cf3518335182ca7f3842d999a4becdd629f0a3a9
AlexandraHessen <xl_energy@mail.ru>
2022-08-05 22:30:06 +0300

    arr_splice

---
+++ arr_splice.js
@@ -0,0 +1,5 @@
+var myFish = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
+var removed = myFish.splice(3, 1);
+
+// removed равен ["mandarin"]
+// myFish равен ["angel", "clown", "drum", "sturgeon"]
