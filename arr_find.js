2d0354fe10bbbdc5f43a214fe79638c66d7dac8d
AlexandraHessen <xl_energy@mail.ru>
2022-08-08 22:01:32 +0300

    arr_find

--- arr_find.js
+++ arr_find.js
@@ -10,3 +10,14 @@ function isPrime(element, index, array) {
   
   console.log([4, 6, 8, 12].find(isPrime)); // undefined, не найдено
   console.log([4, 5, 8, 12].find(isPrime)); // 5
+
+
+  let users = [
+    {id: 1, name: "Вася"},
+    {id: 2, name: "Петя"},
+    {id: 3, name: "Маша"},
+    {id: 1, name: "Маша"}
+  ];
+
+  let user = users.find(((item)=>item.id === 1))
+  let userFilt = users.filter((item)=>item.id === 1)
