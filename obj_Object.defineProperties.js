a2ecb41fabbc86bec70c95ad48ddebc76421361e
AlexandraHessen <xl_energy@mail.ru>
2022-08-09 23:42:47 +0300

    obj_Object.defineProperties

---
+++ obj_Object.defineProperties.js
@@ -0,0 +1,11 @@
+Object.defineProperties(obj, {
+    'property1': {
+      value: true,
+      writable: true
+    },
+    'property2': {
+      value: 'Hello',
+      writable: false
+    }
+    // и т.д.
+  });
