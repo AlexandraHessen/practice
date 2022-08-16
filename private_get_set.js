caa5b3d5be87ac8ced8829d37bf8c55afdc210a9
AlexandraHessen <xl_energy@mail.ru>
2022-08-16 18:35:14 +0300

    private_get_set

---
+++ private_get_set.js
@@ -0,0 +1,18 @@
+class ClassWithPrivateAccessor {
+    #message
+  
+    get #decoratedMessage() {
+      return `✨${this.#message}✨`
+    }
+    set #decoratedMessage(msg) {
+      this.#message = msg
+    }
+  
+    constructor() {
+      this.#decoratedMessage = 'hello world'
+      console.log(this.#decoratedMessage)
+    }
+  }
+  
+  new ClassWithPrivateAccessor();
+  // expected output: "✨hello worl​d✨"
