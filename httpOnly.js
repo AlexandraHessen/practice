31b34b798e8b0645bc2202c2e199264487363dc7
AlexandraHessen <xl_energy@mail.ru>
2022-08-17 21:45:22 +0300

    httpOnly

---
+++ httpOnly.js
@@ -0,0 +1,6 @@
+Set-Cookie: <name>=<value>[; <Max-Age>=<age>]
+`[; expires=<date>][; domain=<domain_name>]
+[; path=<some_path>][; secure][; HttpOnly]
+
+
+Set-Cookie: id=a3fWa; Expires=Wed, 21 Oct 2015 07:28:00 GMT; Secure; HttpOnly
