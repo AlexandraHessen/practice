2b4be2f72e4cc1b02709ffcd539649e1486a4331
AlexandraHessen <xl_energy@mail.ru>
2022-09-04 22:03:52 +0300

    getOwnPropertyDescriptor

---
+++ getOwnPropertyDescriptor.js
@@ -0,0 +1,15 @@
+let user = {
+    name: "John"
+  };
+  
+  let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
+  
+  alert( JSON.stringify(descriptor, null, 2 ) );
+  /* дескриптор свойства:
+  {
+    "value": "John",
+    "writable": true,
+    "enumerable": true,
+    "configurable": true
+  }
+  */
