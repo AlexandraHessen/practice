0b3a3a5ea3a183aafde45d2c5823a9a699dd6b70
AlexandraHessen <xl_energy@mail.ru>
2022-09-05 22:16:01 +0300

    Promise.all

---
+++ Promise.all.js
@@ -0,0 +1,25 @@
+var p1 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 1000, "one");
+  });
+  var p2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000, "two");
+  });
+  var p3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 3000, "three");
+  });
+  var p4 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 4000, "four");
+  });
+  var p5 = new Promise((resolve, reject) => {
+  // Этот промис прервёт Promise.all
+    reject("reject");
+  });
+  
+  Promise.all([p1, p2, p3, p4, p5]).then(value => {
+    console.log(value);
+  }, reason => {
+    console.log(reason)
+  });
+  
+  //Выведет:
+  //"reject"
