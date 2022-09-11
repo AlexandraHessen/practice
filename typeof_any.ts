dbf2e8e17299af671bbbdb69c7da42209511b087
AlexandraHessen <xl_energy@mail.ru>
2022-09-11 22:36:17 +0300

    typeof_any

---
+++ typeof_any.ts
@@ -0,0 +1,11 @@
+let sum: any;
+sum = 1200;
+ 
+if (typeof sum === "number") {
+     
+    let result: number = sum / 12;
+    console.log(result);
+}
+else{
+    console.log("invalid operation");
+}
