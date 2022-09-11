5dc554556d979e79bd7b17dd784760026a03238f
AlexandraHessen <xl_energy@mail.ru>
2022-09-11 22:35:22 +0300

    any

---
+++ any.ts
@@ -0,0 +1,8 @@
+let someVar: any = "hello";
+console.log(someVar);   // сейчас someVar - это string
+someVar = 20; 
+console.log(someVar);   // сейчас someVar - это number
+
+let x;  // тип any
+x = 10; 
+x = "hello";    
