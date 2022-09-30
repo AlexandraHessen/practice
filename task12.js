3ce1007bec4d55da0efd7572e7b8ce45ed5e56a1
AlexandraHessen <xl_energy@mail.ru>
2022-09-30 23:03:43 +0300

    task12

---
+++ task12.js
@@ -0,0 +1,6 @@
+function isAnagram(str1, str2) {
+    return str1.split('').sort().join('') == str2.split('').sort().join('')
+}
+
+console.log(isAnagram('njh', 'jhn'))
+console.log(isAnagram('njht', 'jhn'))
