169921a430d22013c21167ef5b44955f10212ef0
AlexandraHessen <xl_energy@mail.ru>
2022-10-08 23:38:52 +0300

    arrayFromArgs

---
+++ arrayFromArgs.js
@@ -0,0 +1,9 @@
+function arrayFromArgs() {
+    var results = [];
+    for (var i = 0; i < arguments.length; i++) {
+        results.push(arguments[i]);
+    }
+    return results;
+}
+var fruits = arrayFromArgs('Apple', 'Orange', 'Banana');
+console.log(fruits);
