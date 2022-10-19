65efdc5bb7b89c4c0c50fcabc2b8aeacc6c94779
AlexandraHessen <xl_energy@mail.ru>
2022-10-19 23:49:56 +0300

    ClassName

---
+++ ClassName.js
@@ -0,0 +1,10 @@
+function ClassName() {
+    this.svoistvo=value;
+   }
+  
+   ClassName.prototype.metod=function() { } // создаем метод в 
+                                  прототипе который доступен всем экземплярам класса
+  
+   let objClass1 = new ClassName
+   let objClass2 = new ClassName
+   objClass1.metod === bjClass1.metod 
