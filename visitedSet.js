d1ffb4dd8165a02279310abe1990860084d99394
AlexandraHessen <xl_energy@mail.ru>
2022-10-31 21:44:05 +0300

    visitedSet

---
+++ visitedSet.js
@@ -0,0 +1,19 @@
+let visitedSet = new WeakSet();
+
+let john = { name: "John" };
+let pete = { name: "Pete" };
+let mary = { name: "Mary" };
+
+visitedSet.add(john); // John заходил к нам
+visitedSet.add(pete); // потом Pete
+visitedSet.add(john); // John снова
+
+// visitedSet сейчас содержит двух пользователей
+
+// проверим, заходил ли John?
+alert(visitedSet.has(john)); // true
+
+// проверим, заходила ли Mary?
+alert(visitedSet.has(mary)); // false
+
+john = null;
