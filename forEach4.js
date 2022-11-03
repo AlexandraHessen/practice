03f51232be01119fa952a732151807a166877823
AlexandraHessen <xl_energy@mail.ru>
2022-11-03 23:50:44 +0300

    forEach

---
+++ forEach4.js
@@ -0,0 +1,6 @@
+["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+    alert(`${item} имеет позицию ${index} в ${array}`);
+  });
+
+  // Вызов alert для каждого элемента
+["Bilbo", "Gandalf", "Nazgul"].forEach(alert);
