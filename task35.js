a7aa1b4022790ff2892b3b88ff1647d50a6de2e6
AlexandraHessen <xl_energy@mail.ru>
2022-11-26 23:38:26 +0300

    task 35

---
+++ task35.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <script>
+        let user = {
+            5: "John",
+        };
+
+        console.log(
+            Object.keys(user).toString()
+        );
+
+        console.log( Object.keys(user));
+        console.log(5);
+        console.log(Object.keys(user)[0]);
+
+    </script>
+</body>
+
+</html>
