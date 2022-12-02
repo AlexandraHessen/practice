a8f2cb7842e39d3c2dcaab03a58c34b09767fa72
AlexandraHessen <xl_energy@mail.ru>
2022-12-02 23:27:31 +0300

    insertionSort

---
+++ insertionSort2.js
@@ -0,0 +1,10 @@
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      let current = i;
+    
+      while(arr[current - 1] !== undefined && comparator(arr[current], arr[current - 1]) < 0) {
+          swap(arr, current - 1, current);
+          current--;
+      }
+    }
+  }
