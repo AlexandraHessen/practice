0e281895f3a4eaa89e0026bf134c1ac13c9771d8
AlexandraHessen <xl_energy@mail.ru>
2022-12-04 21:50:46 +0300

    sup

---
+++ sup.js
@@ -0,0 +1,5 @@
+var superText = 'надстрочный';
+var subText = 'подстрочный';
+
+document.write('Вот так выглядит ' + superText.sup() + ' текст.');
+document.write('Вот так выглядит ' + subText.sub() + ' текст.');
