a4b87fc2a0042bef4b921776cffea7e1da2ddbfa
AlexandraHessen <xl_energy@mail.ru>
2022-12-04 21:50:06 +0300

    worldString

---
+++ worldString.js
@@ -0,0 +1,6 @@
+var worldString = 'Привет, мир';
+
+document.write(worldString.blink());
+document.write(worldString.bold());
+document.write(worldString.italics());
+document.write(worldString.strike());
