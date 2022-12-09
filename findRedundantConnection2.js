c1db9787a2fc77952745bf0fd3406e8451b9a869
AlexandraHessen <xl_energy@mail.ru>
2022-12-09 23:45:22 +0300

    findRedundantConnection

---
+++ findRedundantConnection2.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+
+ // edges = [[1,2],[1,3],[2,3]]
+ var findRedundantConnection = function(edges) {
+    // i = [1,2]
+    let obj = {};
+
+    let arr = [];
+
+    for (let i = 0; i < edges.length; i++){
+        let node1 = edges[i][0];
+        let node2 = edges[i][1];
+
+        if ( !obj[node1] ) {
+            obj[node1] = 1;
+        } else {
+            obj[node1]++;
+        }
+
+        if ( !obj[node2] ) {
+            obj[node2] = 1;
+        } else {
+            obj[node2]++;
+        }
+
+        if ( obj[node1] > 1 && obj[node2] > 1){
+            // return edges[i];
+            arr.push(edges[i]);
+        }
+    }
+
+    return arr.pop();
+};
