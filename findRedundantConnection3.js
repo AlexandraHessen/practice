df60d7cd632cce7c451e877df4a74e2c86437f4e
AlexandraHessen <xl_energy@mail.ru>
2022-12-09 23:46:59 +0300

    findRedundantConnection

--- findRedundantConnection3.js
+++ findRedundantConnection3.js
@@ -3,43 +3,42 @@
  * @return {number[]}
  */
 
- // edges = [[1,2],[1,3],[2,3]]
- var findRedundantConnection = function(edges) {
+// edges = [[1,2],[1,3],[2,3]]
+var findRedundantConnection = function (edges) {
     const rank = new Map(new Array(edges.length).fill(1).map((_, index) => [index + 1, 1]));
     const relation = new Map(new Array(edges.length).fill(1).map((_, index) => [index + 1, index + 1]));
-    
-    for (let i = 0; i < edges.length; i++){
-      if (isCycle(...edges[i], relation, rank)){
-        return edges[i];
-      }
+
+    for (let i = 0; i < edges.length; i++) {
+        if (isCycle(...edges[i], relation, rank)) {
+            return edges[i];
+        }
     }
-      
-  };
-  
-    function isCycle(node1, node2, relation, rank){
-      const parent1 = find(relation, node1);
-      const parent2 = find(relation, node2);
-      
-      if (parent1 === parent2) {
+
+};
+
+function isCycle(node1, node2, relation, rank) {
+    const parent1 = find(relation, node1);
+    const parent2 = find(relation, node2);
+
+    if (parent1 === parent2) {
         return true;
-      }
-      
-      if ( parent1 >= parent2) {
+    }
+
+    if (parent1 >= parent2) {
         rank.set(parent1, 1 + rank.get(parent2));
         relation.set(parent2, parent1);
-      } else {
+    } else {
         rank.set(parent2, 1 + rank.get(parent1));
         relation.set(parent1, parent2);
-      }
-      
     }
-  
-  function find(relation, node2){
+
+}
+
+function find(relation, node2) {
     let parent = relation.get(node2);
-    while ( parent !== relation.get(parent) ){
-      parent = relation.get(parent);
+    while (parent !== relation.get(parent)) {
+        parent = relation.get(parent);
     }
-    
+
     return parent;
-  }
-  
+}
